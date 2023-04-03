95bbe65b84d12216fe0fb63c7a976ca3d21829d0
FZ2161 <filipzalewski44@gmail.com>
2023-04-03 11:18:22 +0200

    logowanie

---
+++ app.js
@@ -0,0 +1,27 @@
+const login = 'admin'
+const pass = 'admin'
+
+function loginn(){
+    const formLogin = document.getElementById('login').value
+    const formPass = document.getElementById('pass').value
+
+    console.log(formLogin)
+
+    if(formLogin==login && formPass==pass){
+        alert('zalogowano')
+        localStorage.setItem('czyZalogowany', 'true')
+    }else{
+        alert('nie zalogowano')
+        localStorage.setItem('czyZalogowany', 'false')
+    }
+}
+
+
+
+function admin(){
+    const czyZalogowany = localStorage.getItem('czyZalogowany')
+
+    if(czyZalogowany!='true'){
+        window.location.href = 'login.html'
+    }
+}
